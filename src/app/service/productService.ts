ea67e76c9ca85dc602c9d60d98410554cf9f03a5
gokul jayan <gokuljayan494@gmail.com>
2022-12-01 09:18:23 +0530

    fourth

--- src/app/service/productService.ts
+++ src/app/service/productService.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
@@ -11,6 +11,14 @@ export class productService {
   constructor(private http: HttpClient, private router: Router) {}
 
   //   create product
+  // let t: any = ``;
+  //   var headers_object = new HttpHeaders();
+  //   headers_object.append('Content-Type', 'application/json');
+  //   headers_object.append('Authorization', 'Bearer ' + t);
+
+  //   const httpOptions = {
+  //     headers: headers_object,
+  //   };
 
   createProduct(products: {
     name: String;
@@ -77,21 +85,33 @@ export class productService {
       });
   }
   onLogin(data: any) {
-    console.log(`${(data.email, data.password)}`);
-    let login = 'login';
+    let token =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzODQzZTEyNDQwYmFhMjg3ZDY2NmFjMSIsImlhdCI6MTY2OTcxMzgyNH0.6Ms5Ii3gZdBXk-UBs23X_mi3MKEmx60tUwf3RrrJnlM';
     const headers = new HttpHeaders({
-      Authorization: `Bearer ${data.token}`,
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
     });
+
+    const requestOptions = { headers: headers };
+
     this.http
       .post<any>(
         'https://farm-house-reubro.onrender.com/api/v1/admin/login ',
         data,
-        { headers }
+        requestOptions
       )
       .subscribe((res) => {
         console.log(res);
+        console.log(res.headers);
+
         console.log(`-------------`);
         localStorage.setItem('token', res.token);
+        console.log(`-------------`);
+        console.log(`-------------`);
+        console.log(`-------------`);
+        console.log(`-------------`);
+        console.log(`-------------`);
+
         if (!res) {
           console.log(res);
           alert('check admin email and password');
